fa3a6f4fcff6df4cb17f3afa867871efdb662278
Sunny0564 <60807434+Sunny0564@users.noreply.github.com>
2023-02-02 18:21:16 +0530

    Add files via upload

---
+++ LLInsertAny.py
@@ -0,0 +1,57 @@
+class _Node:
+    __slots__ = '_element', '_next'
+
+    def __init__(self, element, next):
+        self._element = element
+        self._next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def isempty(self):
+        return self._size == 0
+
+    def addlast(self, e):
+        newest = _Node(e, None)
+        if self.isempty():
+            self._head = newest
+        else:
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+    def addany(self, e, position):
+        newest = _Node(e, None)
+        p = self._head
+        i = 1
+        while i < position - 1:
+            p = p._next
+            i = i + 1
+        newest._next = p._next
+        p._next = newest
+        self._size += 1
+
+    def display(self):
+        p = self._head
+        while p:
+            print(p._element, end='-->')
+            p = p._next
+        print()
+
+
+L = LinkedList()
+L.addlast(7)
+L.addlast(4)
+L.addlast(12)
+L.addlast(8)
+L.addlast(3)
+L.addany(40, 5)
+L.display()
+print('Size:', len(L))
---
+++ LLRemoveFirst.py
@@ -0,0 +1,59 @@
+class _Node:
+    __slots__ = '_element', '_next'
+
+    def __init__(self, element, next):
+        self._element = element
+        self._next = next
+
+class LinkedList:
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def isempty(self):
+        return self._size == 0
+
+    def addlast(self, e):
+        newest = _Node(e, None)
+        if self.isempty():
+            self._head = newest
+        else:
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+    def removefirst(self):
+        if self.isempty():
+            print('List is empty')
+            return
+        e = self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        if self.isempty():
+            self._tail = None
+        return e
+
+    def display(self):
+        p = self._head
+        while p:
+            print(p._element,end='-->')
+            p = p._next
+        print()
+
+
+L = LinkedList()
+L.addlast(7)
+L.addlast(4)
+L.addlast(12)
+L.addlast(8)
+L.addlast(3)
+L.display()
+print('Size:',len(L))
+ele = L.removefirst()
+L.display()
+print('Size:',len(L))
+print('Element Removed:',ele)
